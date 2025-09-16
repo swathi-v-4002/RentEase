ec67c3649b9dd9cc0d569f08d5adc364f80c84f9
swathi-v-4002 <swathiv4002@gmail.com>
2025-09-16 15:12:47 +0530

    first commit

---
+++ rentease-frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      {/* Add your login form here */}
+    </div>
+  );
+}
+
+// This line is the key!
+export default LoginPage;
---
+++ rentease-frontend/src/pages/RentalPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RentalPage() {
+  return (
+    <div>
+      <h2>Rental Details</h2>
+      {/* This page will display details for a specific rental item */}
+    </div>
+  );
+}
+
+// This is the essential line!
+export default RentalPage;
